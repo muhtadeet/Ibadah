ee52e728589701b67802526e40672ad02903c060
muhtadeet <sheikh.taron@northsouth.edu>
2024-03-26 02:39:35 +0600

    Update Quran.jsx

--- components/Quran.jsx
+++ components/Quran.jsx
@@ -1,13 +1,39 @@
+import { useCallback, useEffect, useMemo, useState } from "react";
+import {
+  View,
+  useColorScheme,
+  ActivityIndicator,
+  SafeAreaView,
+  ScrollView,
+} from "react-native";
+import {
+  PaperProvider,
+  MD3LightTheme,
+  MD3DarkTheme,
+  Appbar,
+  Card,
+  Divider,
+  Text,
+  Icon,
+} from "react-native-paper";
+import { useMaterial3Theme } from "@pchmn/expo-material3-theme";
 import axios from "axios";
-import React, { useCallback, useEffect, useState } from "react";
-import { Text } from "react-native-paper";
 
 const Quran = () => {
+  const colorScheme = useColorScheme();
+  const { theme } = useMaterial3Theme({ fallbackSourceColor: "#37306B" });
+  const paperTheme = useMemo(
+    () =>
+      colorScheme === "dark"
+        ? { ...MD3LightTheme, colors: theme.light }
+        : { ...MD3DarkTheme, colors: theme.dark },
+    [colorScheme, theme]
+  );
   const [quran, setQuran] = useState();
   const getQuran = useCallback(async () => {
     try {
       const response = await axios.get(
-        `http://api.alquran.cloud/v1/quran/en.asad`
+        `http://api.alquran.cloud/v1/quran/quran-uthmani`
       );
 
       if (response.data.data) {
@@ -22,17 +48,109 @@ const Quran = () => {
     getQuran();
   }, [getQuran]);
 
-  console.log(quran.surahs[0].ayahs);
+  if (!quran) {
+    return (
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: theme[colorScheme].onSecondaryContainer,
+        }}
+      >
+        <View
+          style={{
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <ActivityIndicator
+            size="large"
+            color={theme[colorScheme].tertiaryContainer}
+          />
+        </View>
+      </View>
+    );
+  }
+
+  console.log(quran.surahs[1].englishName);
   return (
     <>
-      {/* <Text>{quran[0].ayahs[0].text}</Text>
-      <Text>{quran[0].ayahs[1].text}</Text>
-      <Text>{quran[0].ayahs[2].text}</Text>
-      <Text>{quran[0].ayahs[3].text}</Text>
-      <Text>{quran[0].ayahs[4].text}</Text>
-      <Text>{quran[0].ayahs[5].text}</Text>
-      <Text>{quran[0].ayahs[6].text}</Text>
-      <Text>{quran[0].ayahs[7].text}</Text> */}
+      <PaperProvider theme={paperTheme}>
+        <Appbar.Header
+          mode="center-aligned"
+          style={{
+            backgroundColor: theme[colorScheme].onSecondaryContainer,
+            color: theme[colorScheme].primaryContainer,
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Appbar.Content
+            title=""
+            titleStyle={{
+              color: theme[colorScheme].primaryContainer,
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          />
+        </Appbar.Header>
+        <SafeAreaView
+          style={{
+            backgroundColor: theme[colorScheme].onSecondaryContainer,
+            flex: 1,
+          }}
+        >
+          <ScrollView>
+            <Card elevation={0} style={{ marginHorizontal: 20, marginTop: 20 }}>
+              {/* <Card.Title
+                title="Ramadan Mubarak"
+                titleVariant="headlineSmall"
+              /> */}
+              <Card.Content
+                style={{
+                  flexDirection: "column",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  gap: 20,
+                }}
+              >
+                <Text
+                  variant="displayMedium"
+                  style={{ color: theme[colorScheme].tertiaryContainer }}
+                >
+                  Quran
+                </Text>
+              </Card.Content>
+            </Card>
+            <Card
+              style={{ marginTop: 30, paddingVertical: 5, marginBottom: -10 }}
+            >
+              <Card.Content>
+                <View
+                  style={{
+                    // backgroundColor: theme[colorScheme].onSecondaryContainer,
+                    flex: 1,
+                    marginBottom: 30,
+                  }}
+                >
+                  <View
+                    style={{
+                      marginTop: 20,
+                      display: "flex",
+                      alignItems: "center",
+                      marginBottom: 20,
+                      paddingHorizontal: 45,
+                    }}
+                  ></View>
+                </View>
+              </Card.Content>
+            </Card>
+          </ScrollView>
+        </SafeAreaView>
+      </PaperProvider>
     </>
   );
 };
