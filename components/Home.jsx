438f9973aaafd8cf56122ea4210eb64caacdf1e1
muhtadeet <sheikh.taron@northsouth.edu>
2024-03-27 20:01:13 +0600

    fixed the date which was 1 day ahead

--- components/Home.jsx
+++ components/Home.jsx
@@ -146,7 +146,7 @@ export default Home = () => {
 
       try {
         const response = await axios.get(
-          `https://api.aladhan.com/v1/timingsByAddress`,
+          `https://api.aladhan.com/v1/timingsByAddress?adjustment=-1`,
           { params }
         );
 
